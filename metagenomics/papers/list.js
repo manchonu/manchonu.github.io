cbab135ea739b77b14cd78319b04216d9ef087cf
Kevin M.C. U <manchonu@gmail.com>
2018-11-20 22:06:33 -0500

    Updated all contents
    Updated all contents

---
+++ metagenomics/papers/list.js
@@ -0,0 +1,36 @@
+<script language="Javascript">
+<!--
+	page import="java.io.*"
+
+-->
+</script>
+<HTML>
+    <HEAD>
+        <TITLE>Index of Files</TITLE>
+    </HEAD>
+
+    <BODY>
+        <H1>Index of Files</H1>
+        <% 
+            String file = application.getRealPath("/"); 
+
+            File f = new File(file);
+            String [] fileNames = f.list();
+            File [] fileObjects= f.listFiles();
+        %>
+        <UL>
+        <%
+            for (int i = 0; i < fileObjects.length; i++) {
+                if(!fileObjects[i].isDirectory()){
+        %>
+        <LI>
+          <A HREF="<%= fileNames[i] %>"><%= fileNames[i] %></A>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          (<%= Long.toString(fileObjects[i].length()) %> bytes long)
+        <%
+                }
+            }
+        %>
+        </UL>
+    </BODY>
+</HTML>
